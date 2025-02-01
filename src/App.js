2cd66b50c667e081fad41e1dfae96a92977dd8d1
Sandeep <sandeepdewangan7@gmail.com>
2025-02-01 13:20:51 +0530

    setState bool variable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const message = ["React is awesome", "React is good", "React is nice"];
 
 export default function App() {
   const [step, setStep] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
 
   function onNextClick() {
     if (step < 2) setStep(step + 1);
@@ -14,16 +15,21 @@ export default function App() {
 
   return (
     <div>
-      <div>
-        <h2 style={step === 0 ? { color: "red" } : { color: "" }}>1</h2>
-        <h2 style={step === 1 ? { color: "red" } : { color: "" }}>2</h2>
-        <h2 style={step === 2 ? { color: "red" } : { color: "" }}>3</h2>
-      </div>
-      <div>{message[step]}</div>
-      <div>
-        <button onClick={() => onNextClick()}>Next</button>
-        <button onClick={() => onPrevClick()}>Previous</button>
-      </div>
+      <button onClick={() => setIsOpen(!isOpen)}>Close</button>
+      {isOpen && (
+        <div>
+          <div>
+            <h2 style={step === 0 ? { color: "red" } : { color: "" }}>1</h2>
+            <h2 style={step === 1 ? { color: "red" } : { color: "" }}>2</h2>
+            <h2 style={step === 2 ? { color: "red" } : { color: "" }}>3</h2>
+          </div>
+          <div>{message[step]}</div>
+          <div>
+            <button onClick={() => onNextClick()}>Next</button>
+            <button onClick={() => onPrevClick()}>Previous</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
